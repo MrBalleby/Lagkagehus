6552d99c55b10e078e362df01c0b877a701bb390
Martin Jørgensen <satoz.game@gmail.com>
2021-09-07 13:21:46 +0200

    Update pictures and pruduct list

--- LagkageHus/LagkageHusWebApp/Pages/shoppage.cshtml.cs
+++ LagkageHus/LagkageHusWebApp/Pages/shoppage.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LagkageHusWebApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -11,6 +12,19 @@ namespace LagkageHusWebApp.Pages
     {
         public void OnGet()
         {
+
+            var products = new List<Product>() {
+                new (){ Id = 1, Name="kagemand"},
+                new Product(){ Id = 2, Name="jordbaer", Price=20},
+                new Product(){ Id = 3, Name="cheesecake", Price=20},
+                new Product(){ Id = 4, Name="citronkage", Price=20},
+                new Product(){ Id = 5, Name="rumkugler", Price=20},
+                new Product(){ Id = 6, Name="chokolade", Price=20}
+
+
+            };
+
+
         }
     }
 }
