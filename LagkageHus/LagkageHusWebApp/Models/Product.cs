5f9eb3f3f4d1d94e5cb965fcc5b5b3c4dd61835a
Aputsiaq Lindhardt <51225@airgreenland.gl>
2021-09-08 09:07:48 +0200

    Tilføjet Id på Product

--- LagkageHus/LagkageHusWebApp/Models/Product.cs
+++ LagkageHus/LagkageHusWebApp/Models/Product.cs
@@ -7,6 +7,7 @@ namespace LagkageHusWebApp.Models
 {
     public class Product
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public double Price { get; set; }
         public string Image { get; set; }
